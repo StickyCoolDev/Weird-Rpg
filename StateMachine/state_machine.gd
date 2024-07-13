02ffcfa91dee939708a69cf259a5cad75fcde56a
StickyCoolDev <hujifa.info@gmail.com>
2024-07-13 21:45:58 +0600

    update state_machine.gd

--- StateMachine/state_machine.gd
+++ StateMachine/state_machine.gd
@@ -6,14 +6,11 @@ var starting_state: State
 
 var current_state: State
 
-# Initialize the state machine by giving each child state a reference to the
-# parent object it belongs to and enter the default starting_state.
-func init(parent: Player) -> void:
-for child in get_children():
-child.parent = parent
 
-# Initialize to the default state
-change_state(starting_state)
+func init(parent: Player) -> void:
+  for child in get_children():
+    child.parent = parent
+  change_state(starting_state)
 
 # Change to the new state by first calling any exit logic on the current state.
 func change_state(new_state: State) -> void:
