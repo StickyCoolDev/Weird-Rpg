c51c7b8554e0e6f47f0f0ce2ba37c6bb628ae784
StickyCoolDev <hujifa.info@gmail.com>
2024-07-14 20:27:55 +0600

    update state_machine.gd

--- StateMachine/state_machine.gd
+++ StateMachine/state_machine.gd
@@ -12,20 +12,18 @@ func init(parent: Player) -> void:
     child.parent = parent
   change_state(starting_state)
 
-# Change to the new state by first calling any exit logic on the current state.
+
 func change_state(new_state: State) -> void:
-if current_state:
-current_state.exit()
+  if current_state:
+    current_state.exit()
+  current_state = new_state
+  current_state.enter()
 
-current_state = new_state
-current_state.enter()
 
-# Pass through functions for the Player to call,
-# handling state changes as needed.
 func process_physics(delta: float) -> void:
-var new_state = current_state.process_physics(delta)
-if new_state:
-change_state(new_state)
+  var new_state = current_state.process_physics(delta)
+  if new_state:
+    change_state(new_state)
 
 func process_input(event: InputEvent) -> void:
 var new_state = current_state.process_input(event)
