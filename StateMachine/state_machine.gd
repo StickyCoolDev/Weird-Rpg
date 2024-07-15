88cea37818187c567b72dea2e3ab71d9b93b7d45
StickyCoolDev <hujifa.info@gmail.com>
2024-07-15 14:36:24 +0600

    update state_machine.gd

--- StateMachine/state_machine.gd
+++ StateMachine/state_machine.gd
@@ -26,11 +26,11 @@ func process_physics(delta: float) -> void:
     change_state(new_state)
 
 func process_input(event: InputEvent) -> void:
-var new_state = current_state.process_input(event)
-if new_state:
-change_state(new_state)
+  var new_state = current_state.process_input(event)
+  if new_state:
+    change_state(new_state)
 
 func process_frame(delta: float) -> void:
-var new_state = current_state.process_frame(delta)
-if new_state:
-change_state(new_state)
+  var new_state = current_state.process_frame(delta)
+  if new_state:
+    change_state(new_state)
